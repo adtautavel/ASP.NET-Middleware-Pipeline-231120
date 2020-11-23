ed5649ba4545b68f7674eaf2b31b94677af46e74
adtautavel <adtautavel@yahoo.fr>
2020-11-23 11:27:35 +0100

    "Using app.Run( async context..."

--- ASP_MW_Pipeline_quest231120/Startup.cs
+++ ASP_MW_Pipeline_quest231120/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -31,9 +32,14 @@ namespace ASP_MW_Pipeline_quest231120
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            env.ApplicationName = "AllowQuestConfiguration";
+          
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.Run(async context =>
+           {
+               await context.Response.WriteAsync("Hello World!");
+           });
         }
     }
 }
